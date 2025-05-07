f5d3dcf61043323936e2ba5e048e084426070f5c
crowplexus <45212377+crowplexus@users.noreply.github.com>
2025-05-06 23:26:11 -0400

    i forgot to change the comments, anyway

--- scripts/gameplay/note_data.gd
+++ scripts/gameplay/note_data.gd
@@ -31,7 +31,7 @@ const ROWS_PER_BEAT: int = 48 ## Fixed-point time/beat representation from SM.
 const ROWS_PER_MEASURE: int = ROWS_PER_BEAT * 4 ## Rows used in a measure
 const QUANT_LIST: Array[int] = [4, 8, 12, 16, 24, 32, 48, 64, 192] ## List of quant types.
 
-## Retrieve the proper quantized NoteType for the note.
+## Retrieve a quantized note row for the note.
 static func get_note_quant(row: int) -> int:
 	for quant: int in QUANT_LIST:
 		if row % (ROWS_PER_MEASURE % quant) == 0:
@@ -50,21 +50,13 @@ static func secs_to_row(p_time: float, p_bpm: float = Conductor.bpm) -> int:
 static func beat_to_row(p_beat: float) -> int:
 	return round(p_beat * ROWS_PER_BEAT)
 
-## [code]Conductor.beat_to_row[/code] but in reverse.
+## [code]NoteData.beat_to_row[/code] but in reverse.
 static func row_to_beat(p_row: int) -> float:
 	return p_row / ROWS_PER_BEAT
 
-## [code]Conductor.secs_to_row[/code] but in reverse.
+## [code]NoteData.secs_to_row[/code] but in reverse.
 static func row_to_secs(p_row: int, p_bpm: float = Conductor.bpm) -> float:
 	return Conductor.get_time(p_row / ROWS_PER_BEAT, p_bpm)
-
-## Convert the beat into a note row.
-static func beat_to_note_row(f_beat: float) -> int:
-	return roundi(f_beat * ROWS_PER_BEAT)
-
-## Convert the note row to a beat.
-static func note_row_to_beat(i_row: int) -> float:
-	return float(i_row) / ROWS_PER_BEAT
 
 ## Snaps a row to the nearest quant.
 static func snap_row_to_quant(row: int, quant: int) -> int:
